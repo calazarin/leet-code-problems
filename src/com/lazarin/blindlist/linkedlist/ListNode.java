32233181f69e761829e9f490aa7a03319f65ca6a
Carlos Lazarin <carlos.joia.mail@gmail.com>
2022-11-28 22:03:29 -0300

    Adding two numbers

--- src/com/lazarin/blindlist/linkedlist/ListNode.java
+++ src/com/lazarin/blindlist/linkedlist/ListNode.java
@@ -3,7 +3,7 @@ package com.lazarin.blindlist.linkedlist;
 public class ListNode {
     public  Integer val;
     public ListNode next;
-    ListNode() {}
+    public ListNode() {}
     public ListNode(int val) { this.val = val; }
     ListNode(Integer val, ListNode next) { this.val = val; this.next = next; }
 }
