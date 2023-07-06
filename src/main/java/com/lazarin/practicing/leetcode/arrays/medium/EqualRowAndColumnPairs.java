9070b1ede458b981063f8e5fd1dd684d1abf2045
Carlos Lazarin <carlos.joia.mail@gmail.com>
2023-07-05 21:11:58 -0300

    New array problem

--- src/main/java/com/lazarin/practicing/leetcode/arrays/medium/EqualRowAndColumnPairs.java
+++ src/main/java/com/lazarin/practicing/leetcode/arrays/medium/EqualRowAndColumnPairs.java
@@ -1,43 +1,59 @@
 package com.lazarin.practicing.leetcode.arrays.medium;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 //https://leetcode.com/problems/equal-row-and-column-pairs
 public class EqualRowAndColumnPairs {
 
+    /* brute force
+      T: O(n^3)
+      S: O(1)
+     */
     public int equalPairs(int[][] grid) {
-        int answer = 0;
-        List<String> rowSet = new ArrayList<>();
-        Set<String> colSet = new HashSet<>();
+        int count = 0;
 
-        for(int i = 0; i < grid.length; i++){ //O (n*m)
-            StringBuilder row = new StringBuilder();
-            for(int j = 0; j < grid[i].length; j++){
-                row.append(grid[i][j]+",");
-            }
-            rowSet.add(row.toString());
-        }
+        for(int r = 0; r < grid.length; r++){
+            for(int c = 0; c < grid.length; c++){
 
-        for(int i = 0; i < grid.length; i++){ //O (n*m)
+                boolean valid = true;
+                for(int i = 0; i < grid.length; i++){
 
-            StringBuilder column = new StringBuilder();
+                    if(grid[r][i] != grid[i][c]){
+                        valid = false;
+                        break;
+                    }
 
-            for(int j = 0; j < grid[i].length; j++){
-                column.append(grid[j][i]+",");
-            }
-
-            colSet.add(column.toString());
-        }
-
-        for(String row : rowSet){
-            if(colSet.contains(row)){
-                answer++;
+                }
+                if(valid) count++;
             }
         }
 
-        return answer;
+        return count;
+    }
+
+    /* optimal
+     T: O(n^2)
+    S: O(n^2)
+    */
+    public int equalPairs2(int[][] grid) {
+        int counter = 0;
+        //arrays cannot be used as keys so we need to use Strings
+        Map<String, Integer> rowCounter = new HashMap<>();
+
+        for(int[] row: grid){
+            String rowString = Arrays.toString(row);
+            rowCounter.put(rowString, rowCounter.getOrDefault(rowString, 0) + 1);
+        }
+
+        for(int c  = 0; c < grid.length; c++){
+            int[] column = new int[grid.length];
+            for(int r = 0; r < grid.length; r++){
+                column[r] = grid[r][c];
+            }
+            counter += rowCounter.getOrDefault(Arrays.toString(column), 0);
+        }
+        return counter;
     }
 }
--- src/test/java/com/lazarin/practicing/leetcode/arrays/medium/EqualRowAndColumnPairsTest.java
+++ src/test/java/com/lazarin/practicing/leetcode/arrays/medium/EqualRowAndColumnPairsTest.java
@@ -17,6 +17,18 @@ public class EqualRowAndColumnPairsTest {
 
         assertThat(solution.equalPairs(new int[][]{{3,2,1},{1,7,6}, {2,7,7}}), is(1));
         assertThat(solution.equalPairs(new int[][]{{3,1,2,2},{1,4,4,5},{2,4,2,2},{2,4,2,2}}), is(3));
+        assertThat(solution.equalPairs(new int[][]{{3,1,2,2},{1,4,4,4},{2,4,2,2},{2,5,2,2}}), is(3));
+
+    }
+
+    @Test
+    public void shouldReturnExpectedAnswers_approach2(){
+
+        EqualRowAndColumnPairs solution = new EqualRowAndColumnPairs();
+
+        assertThat(solution.equalPairs2(new int[][]{{3,2,1},{1,7,6}, {2,7,7}}), is(1));
+        assertThat(solution.equalPairs2(new int[][]{{3,1,2,2},{1,4,4,5},{2,4,2,2},{2,4,2,2}}), is(3));
+        assertThat(solution.equalPairs2(new int[][]{{3,1,2,2},{1,4,4,4},{2,4,2,2},{2,5,2,2}}), is(3));
 
     }
 }
